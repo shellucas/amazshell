c4f158c86b62f5a8fc0484984547d524ffe038b5
Shelby Hendrickx <shelby.hendrickx@hotmail.com>
2019-01-03 16:13:07 +0100

    updated gitignore and added models for products and categories

--- amazshell/settings.py
+++ amazshell/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Apps
+
+    # Local Apps
     'shop.apps.ShopConfig',
 ]
 
