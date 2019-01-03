d5d44abd8dcfae79d2ca620caed267f96cc79a3f
Shelby Hendrickx <shelby.hendrickx@hotmail.com>
2019-01-03 18:03:21 +0100

    added navigation bar and changd media url settings

--- shop/urls.py
+++ shop/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -18,3 +20,6 @@ urlpatterns = [
     path('<int:id>/<slug:slug>/', views.product_detail,
          name='product_detail'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
